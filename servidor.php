0c2f29e2416f0d6a1588ff8dad47dc007c6bdc7d
nxbre <84987702+nxbre@users.noreply.github.com>
2021-07-30 23:18:23 -0300

    avaliação2

--- servidor.php
+++ servidor.php
@@ -1,1 +1,12 @@
+<?php
+  try {
+    $username = "root";
+    $password = "";
 
+    $pdo = new PDO(
+      'mysql:dbname=pweb2;host=localhost', $username, $password);
+  } catch(PDOException $e) {
+    echo 'Erro Encontrado: ' . $e->getMessage();
+    exit();
+  }
+?>
