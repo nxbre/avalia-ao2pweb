d0ff5fd3362f4bf3feacb14f277fd40bea1e5377
nxbre <84987702+nxbre@users.noreply.github.com>
2021-07-30 23:27:11 -0300

    avaliação2

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE pweb2;
-USE pweb2;
+CREATE DATABASE ava2pweb;
+    USE ava2pweb;
 
-CREATE TABLE usuario(
-    nome VARCHAR(50) NOT NULL,
-    nomeusuario VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    senha VARCHAR(32) NOT NULL, 
-    CONSTRAINT PRIMARY KEY(nomeusuario, email)
-);
+    CREATE TABLE usuario(
+        nome VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        nickname VARCHAR(50) NOT NULL,
+        pass VARCHAR(32) NOT NULL, 
+        CONSTRAINT PRIMARY KEY(nickname, email)
+    );
