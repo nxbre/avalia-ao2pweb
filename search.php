7235c70acc395a76d3e9bf8c72c8cbc19e7a2a37
nxbre <84987702+nxbre@users.noreply.github.com>
2021-07-30 23:23:37 -0300

    avaliação2

--- search.php
+++ search.php
@@ -1,1 +1,34 @@
+<?php
+    include('servidor.php');
 
+    if(empty(
+      $_POST['search'])){
+      header('Location: list.php');
+      exit();
+    }
+
+    $buscar = $_POST['search'];
+
+    $cmd = $pdo->prepare(
+      "SELECT email, nickname FROM usuario WHERE email = :sc or nickname = :sc");
+
+    $cmd->bindparam(
+      ":sc", $buscar);
+
+    $cmd->execute();
+
+    if($cmd->rowCount() == 0){
+        echo "Conta não cadastrada.<br>";
+    }else{
+        $resultado = $cmd->fetch(PDO::FETCH_ASSOC);
+        echo "Conta:<br>";
+        foreach ($resultado as $key => $value) {
+            echo $key.": ".$value."<br>";
+        }
+        echo "<br>";
+    }
+
+    ?>
+<div><button><a href="list.php">Voltar a página anterior</a></button></div>
+    <?php
+?>
